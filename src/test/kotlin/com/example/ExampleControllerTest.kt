d1a3ec66fa9edb0e0a8dfc0422484bd619597dc3
JSS-DRYP <joakim@drypdata.com>
2023-06-18 21:13:56 +0200

    ✅ added test for example controller

---
+++ src/test/kotlin/com/example/ExampleControllerTest.kt
@@ -0,0 +1,25 @@
+package com.example
+
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+class ExampleControllerTest {
+
+    @Inject
+    @field:Client("/example")
+    lateinit var httpClient: HttpClient
+
+    @Test
+    fun testIndex() {
+        val response = httpClient.toBlocking().exchange("/", String::class.java)
+
+        assertEquals(HttpStatus.OK, response.status)
+        assertEquals("22", response.body())
+    }
+}
