1e223e60750e9312d6d29313f2a34a57ba3fa21c
JSS-DRYP <joakim@drypdata.com>
2023-06-21 00:37:12 +0200

    ✅ added test for user controller

---
+++ src/test/kotlin/com/splitnice/controllers/UserControllerTest.kt
@@ -0,0 +1,25 @@
+package com.splitnice.controllers
+
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+class UserControllerTest {
+
+    @Inject
+    @field:Client("/user")
+    lateinit var httpClient: HttpClient
+
+    @Test
+    fun testIndex() {
+        val response = httpClient.toBlocking().exchange("/1", String::class.java)
+
+        assertEquals(HttpStatus.OK, response.status)
+        assertEquals("{\"id\":1,\"name\":\"John\",\"email\":\"john@mail.com\"}", response.body())
+    }
+}
