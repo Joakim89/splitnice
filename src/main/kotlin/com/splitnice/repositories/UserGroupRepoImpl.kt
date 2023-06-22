38dcce8453768f390bc70edc72805719438d1e76
JSS-DRYP <joakim@drypdata.com>
2023-06-22 22:57:41 +0200

    🚧 working on user group repo

---
+++ src/main/kotlin/com/splitnice/repositories/UserGroupRepoImpl.kt
@@ -0,0 +1,17 @@
+package com.splitnice.repositories
+
+import com.splitnice.domain.UserGroup
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
+
+@Singleton
+class UserGroupRepoImpl @Inject constructor(private val dbConnector: DBConnector) : UserGroupRepo {
+    override fun createUserGroup(userGroup: UserGroup) {
+        val inputQuery = "INSERT INTO splitnice.user_groups (name, description)\n" +
+                         "VALUES ('${userGroup.name}', '${userGroup.description}');"
+
+        val result = dbConnector.executeUpdateQuery(inputQuery)
+
+        //TODO: update foreign keys in users table
+    }
+}
