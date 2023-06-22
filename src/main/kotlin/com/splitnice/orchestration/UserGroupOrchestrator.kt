000dbf269938620f7c7eb00cb3abd0a946b4ab15
JSS-DRYP <joakim@drypdata.com>
2023-06-22 22:56:48 +0200

    ♻️ reformat

--- src/main/kotlin/com/splitnice/orchestration/UserGroupOrchestrator.kt
+++ src/main/kotlin/com/splitnice/orchestration/UserGroupOrchestrator.kt
@@ -1,18 +1,25 @@
 package com.splitnice.orchestration
 
 import com.splitnice.controllers.UserGroupApiDto
-import com.splitnice.domain.Expense
 import com.splitnice.domain.User
 import com.splitnice.domain.UserGroup
+import com.splitnice.repositories.UserGroupRepo
 import com.splitnice.repositories.UserRepo
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
 
 @Singleton
-class UserGroupOrchestrator @Inject constructor(private val userRepo: UserRepo) {
+class UserGroupOrchestrator @Inject constructor(private val userRepo: UserRepo, private val userGroupRepo: UserGroupRepo) {
     fun createUserGroup(userGroupApiDto: UserGroupApiDto){
         val users = getUsersByIds(userGroupApiDto.userIds)
-        val userGroup = UserGroup(name = userGroupApiDto.name, users = users, expenses = listOf())
+        val userGroup = UserGroup(
+            name = userGroupApiDto.name,
+            description = userGroupApiDto.description,
+            users = users,
+            expenses = listOf()
+        )
+
+        userGroupRepo.createUserGroup(userGroup)
         //TODO: create repo method to create a user group from a domain object
     }
 
