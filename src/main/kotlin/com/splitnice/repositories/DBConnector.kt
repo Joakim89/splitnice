5ca3847cc6619eb5d5b60f156cc9d025c24a85b8
JSS-DRYP <joakim@drypdata.com>
2023-06-23 01:47:26 +0200

    🎨 created interface for db connector

--- src/main/kotlin/com/splitnice/repositories/DBConnector.kt
+++ src/main/kotlin/com/splitnice/repositories/DBConnector.kt
@@ -1,48 +1,9 @@
 package com.splitnice.repositories
 
-import io.micronaut.context.annotation.Value
-import jakarta.inject.Inject
-import jakarta.inject.Singleton
-import java.sql.Connection
-import java.sql.DriverManager
 import java.sql.ResultSet
-import java.sql.Statement
 
-@Singleton
-class DBConnector @Inject constructor(
-        @Value("\${datasources.default.url}") url: String,
-        @Value("\${datasources.default.username}") username: String,
-        @Value("\${datasources.default.password}") password: String) {
+interface DBConnector {
+    fun getResultFromQuery(inputQuery: String): ResultSet
 
-    private val connection: Connection
-
-    init {
-        connection = DriverManager.getConnection(url, username, password)
-    }
-
-    fun getResultFromQuery(inputQuery: String): ResultSet {
-
-        val query = connection.prepareStatement(inputQuery)
-
-        val result = query.executeQuery()
-
-        result.next()
-
-        return result
-
-    }
-
-    fun executeUpdateQuery(inputQuery: String): Int {
-        val query = connection.prepareStatement(inputQuery, Statement.RETURN_GENERATED_KEYS)
-
-        query.executeUpdate()
-
-        val idQuery = connection.prepareStatement("SELECT LAST_INSERT_ID() AS id")
-
-        val idResult = idQuery.executeQuery()
-        idResult.next()
-        val id = idResult.getInt("id")
-
-        return id
-    }
+    fun executeUpdateQuery(inputQuery: String): Int
 }
